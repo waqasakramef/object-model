5370915afc8e5f89ea4d8fa2c5e6ba13dbb2ec6d
ahmad-bappi <ahmad.bappi@expertflow.com>
2021-06-21 08:19:00 +0500

    v-1.3.7

--- src/main/java/com/ef/cim/objectmodel/Resources.java
+++ src/main/java/com/ef/cim/objectmodel/Resources.java
@@ -1,17 +1,18 @@
 package com.ef.cim.objectmodel;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class Resources {
-    @JsonIgnore
     private List<Resource> Resources;
 
+    @JsonProperty("Resources")
     public List<Resource> getResources() {
         return Resources;
     }
 
-    public void setResources(List<Resource> resources) {
-        Resources = resources;
+    @JsonProperty("Resources")
+    public void setResources(List<Resource> Resources) {
+        this.Resources = Resources;
     }
 }
