1a3653b0c9d90d30bcc9efd80f08c806fb18c2cb
nabeel.ahmad <nabeel.ahmad@expertflow.com>
2022-01-12 17:44:49 +0500

    Added Scopes list in keycloak resource

--- src/main/java/com/ef/cim/objectmodel/Resource.java
+++ src/main/java/com/ef/cim/objectmodel/Resource.java
@@ -1,11 +1,33 @@
 package com.ef.cim.objectmodel;
 
+import java.util.List;
 import java.util.UUID;
 
 public class Resource {
     private UUID rsid;
     private String rsname;
+    private List<String> scopes;
 
+    public List<String> getScopes() {
+        return scopes;
+    }
+
+    public void setScopes(List<String> scopes) {
+        this.scopes = scopes;
+    }
+
+
+    public boolean containsScope(String scope) {
+        return this.scopes.contains(scope);
+    }
+
+    public boolean addScope(String scope) {
+        return this.scopes.add(scope);
+    }
+
+    public boolean removeScope(String scope) {
+        return this.scopes.remove(scope);
+    }
     public UUID getRsid() {
         return rsid;
     }
